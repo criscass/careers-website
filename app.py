ab5c98e9ef8f9b970f513f42891a41dd241ab332
criscass <71939926+criscass@users.noreply.github.com>
2024-01-01 23:18:12 +0000

    Add styling, homepage and job titles

--- app.py
+++ app.py
@@ -1,10 +1,39 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+JOBS = [
+    {
+        'id': 1,
+        'title': 'Python Developer',
+        'location': 'New York',
+        'salary': '$100,000'
+    },
+    {
+        'id': 2,
+        'title': 'Frontend Developer',
+        'location': 'San Francisco',
+        'salary': '$110,000'
+    },
+    {
+        'id': 3,
+        'title': 'Backend Developer',
+        'location': 'Los Angeles',
+        'salary': '$120,000'
+    },{
+      'id': 4,
+      'title': 'Cloud Engineer',
+      'location': 'Pasadina'
+  }
+]
+
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World Mate!</p>"
+    return render_template("home.html", jobs=JOBS)
+
+@app.route("/jobs")
+def list_jobs():
+  return jsonify(JOBS)
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
